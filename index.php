a070413624f271cbc73ce4bb075c28cd4fc0842f
Hilary <hilary.m.johnson@gmail.com>
2014-09-13 15:47:41 -0400

    Updated index.php with rough draft of p1

--- index.php
+++ index.php
@@ -1,1 +1,81 @@
-# index.php for P1
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link href="images/favicon.ico" rel="icon">
+    <title>Hilary Johnson - CSCI E-15 P1</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/jumbotron-narrow.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+    <div class="container">
+        <div class="jumbotron">           
+            <img src="images/hilary_web.jpg" alt="Photo of Hilary" class="img-rounded">
+            <h1>Hilary Johnson</h1>
+            <p>M.S. Candidate, Human Factors in Information Design, Bentley University<br>
+           M.L.A Candidate, Digital Media Arts and Instructional Design, Harvard Extension School<br>
+            Program Coordinator, Reischauer Institute of Japanese Studies, Harvard University</p>
+        </div>
+        <div class="row marketing projects">
+          
+                <div class="col-md-4">
+                    <h2>Project 2</h2>
+                    <p>View: <a href="http://p2.hilaryjohnson.me">link</a> / Github: <a href="https://github.com/hilaryj/p2">link</a></p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Project 3</h2>
+                    <p>View: <a href="http://p3.hilaryjohnson.me">link</a> / Github: <a href="https://github.com/hilaryj/p3">link</a></p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Project 4</h2>
+                    <p>View: <a href="http://p4.hilaryjohnson.me">link</a> / Github: <a href="https://github.com/hilaryj/p4">link</a></p>
+                </div>
+          
+        </div>
+        <hr>
+        <div class="row marketing">
+            <div class="col-md-6">
+                <h4>In 2+ sentences describe who you are and what you do.</h4>
+                <p>Studying Human Factors in Info Design at Bentley part-time, while working at Harvard at a center for Japanese Studies.</p>
+                <h4>Why are you taking this course?</h4>
+
+                <p>I want to learn server-side web development. </p>
+
+                <h4>What previous experience do you have with HTML/CSS?</h4>
+
+                <p>I've taken Jen Kramer's DGMD E-20 &amp; E-25, and CSCI E-12 with David Heitmeyer.  I've also studied HTML and CSS through Codecademy, Codeschool and Team Treehouse.<br><strong>tl;dr:</strong> intermediate-level HTML, CSS</p>
+
+                <h4>What previous experience do you have with programming?</h4>
+
+                <p>1 year of Java in high school.  It was the most popular class in the school.  It was a school for nerds...! </p>
+            </div>
+
+            <div class="col-md-6">
+                <h4>Have you worked with PHP before? If so, to what
+                extent?</h4>
+
+                <p>We covered a little PHP in CSCI E-12.  I also beta tested the PHP course for Women's Coding Collective.  I've also experimented with PHP a bit while tweaking WordPress themes.</p>
+
+                <h4>Are you working on a Mac, PC, or some other platform?</h4>
+
+                <p>Both!</p>
+
+                <h4>Anything else we should know?</h4>
+
+                <p>Excited for class!</p>
+            </div>
+        </div>
+        <hr>
+        <div class="footer">
+            <p>© Hilary Johnson 2014</p>
+        </div>
+    </div><!-- /container -->
+</body>
+</html>
